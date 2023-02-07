d5d90d660000a9d4b9d0e593772cf36c3b9aaa25
Rafhael Oliveira <rafhaelxd@gmail.com>
2023-02-07 14:46:55 -0300

    Seeds do model user

---
+++ database/seeders/User.js
@@ -0,0 +1,26 @@
+module.exports = {
+    up: async (queryInterface) => {
+      await queryInterface.bulkInsert('users', [
+        {
+          username: 'Tester1',
+          role: 'admin',
+          email: 'admin@admin.com',
+          password: 'B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu',
+          balance: 0,
+          hasPremium: true,
+        },
+        {
+          username: 'Tester2',
+          role: 'user',
+          email: 'user@user.com',
+          password: 'B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu', 
+          balance: 0,
+          hasPremium: false,
+        },
+      ], {});
+    },
+  
+    down: async (queryInterface) => {
+      await queryInterface.bulkDelete('users', null, {});
+    },
+  };
