6c1637dfafa9592c60fa7e9bdec60990a614dd8c
Rafhael Oliveira <rafhaelxd@gmail.com>
2023-02-07 14:13:57 -0300

    Sequelize: Model database User.ts

---
+++ database/models/User.ts
@@ -0,0 +1,48 @@
+import { Sequelize, Model, INTEGER, STRING } from 'sequelize';
+import * as config from '../config/database';
+
+const sequelize = new Sequelize(config)
+
+class Users extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+  balance!: number;
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING(30),
+    allowNull: false,
+  },
+  role: {
+    type: STRING(30),
+    allowNull: false,
+  },
+  email: {
+    type: STRING(100),
+    allowNull: false,
+  },
+  password: {
+    type: STRING(100),
+    allowNull: false,
+  },
+  balance: {
+    type: INTEGER,
+    allowNull: false,
+  }
+}, {
+  sequelize: sequelize,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Users;
