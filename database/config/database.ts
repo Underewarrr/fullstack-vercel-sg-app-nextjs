772765c1af7e4d9c95224b25bd30c5fa70a7062a
Rafhael Oliveira <rafhaelxd@gmail.com>
2023-02-07 14:08:30 -0300

    Configura sequelize npm commands,  Config database dotenv
    Disponivel o acesso a database usando o nextJS.

---
+++ database/config/database.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+const mysql = require('mysql2');
+
+const config: Options = {
+  username: process.env.DB_USER || '',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || '',
+  host: process.env.DB_HOST || 'www.db4free.net',
+ // port: Number(process.env.DB_PORT) || 3002,
+  dialect: 'mysql',
+  dialectModule: mysql, // Needed to fix sequelize issues with WebPack
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
+}
+
+module.exports = config;
