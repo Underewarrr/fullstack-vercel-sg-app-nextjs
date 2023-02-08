3c060de7a3cc95b0114f7f377f1616e3a64db5de
Rafhael Oliveira <rafhaelxd@gmail.com>
2023-02-07 23:24:18 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
       return [
         {
           // matching all API routes
-          source: "/pages/api/:path*",
+          source: "/pages/api/*",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Origin", value: "*" },
